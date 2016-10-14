ee8ab26a70ec2a7f1baf2091ca45fa1c0f8f2906
Mary Danielle C. Amora <mcamora@up.edu.ph>
2016-10-14 10:56:29 +0800

    Update LListTest.java

--- LList/LListTest.java
+++ LList/LListTest.java
@@ -1,27 +1,88 @@
 import java.util.*;
+import junit.framework.TestCase;
 
-public class LListTest {
-  
-  public static void main(String[] args) {    
-    LList l = new LList();
-    l.add("A");
-    l.add("B");
-    l.add("C");
-    
-    Iterator it = l.iterator();
-    System.out.println(it.next());
-    System.out.println(it.next());  
-    System.out.println(it.next()); 
-    it.remove();
-    
-    while (it.hasNext()) {
-      System.out.println(it.next()); 
-    }
-    
-    for (Object el : l) {
-      System.out.println(el);
+
+public class LListTest extends TestCase {
+ public void testNext(){
+    LList<String> list = new LList<String>();
+    list.add("Q");
+    list.add("R");
+    list.add("S");
+
+    Iterator iter = list.iterator();
+
+    assertEquals("added Q", "Q", iter.next());
+    assertEquals("added R", "R", iter.next());
+    assertEquals("added S", "S", iter.next());
     }
 
-  }
-  
+ public void testRemove(){
+   LList<String> list = new LList<String>();
+   list.add("Q");
+   list.add("R");
+   list.add("S");
+
+   Iterator iter = list.iterator();
+   iter.next();
+   iter.remove();
+   assertEquals("removed Q", "R", iter.next());
+   iter.remove();
+   assertEquals("removed R", "S", iter.next());
+ }
+
+ public void testIllegalStateException(){
+   LList<String> list = new LList<String>();
+   Iterator iter = list.iterator();
+
+  try {
+      iter.remove();
+      fail("Should have thrown an exception!");
+    } catch (IllegalStateException e) {
+      //
+    }
+ }
+
+ public void testISEAgain(){
+ LList<String> list = new LList<String>();
+
+
+   list.add("Q");
+   list.add("R");
+   list.add("S");
+
+  Iterator iter = list.iterator();
+   System.out.println(iter.next());
+   System.out.println(iter.next());
+   System.out.println(iter.next());
+
+   iter.remove();
+
+  try {
+      iter.remove();
+      fail("Should have thrown an exception!");
+    } catch (IllegalStateException e) {
+      //
+    }
+ }
+
+ public void testNosuchElement(){
+    LList<String> list = new LList<String>();
+   list.add("Q");
+   list.add("R");
+   list.add("S");
+
+  Iterator iter = list.iterator();
+   System.out.println(iter.next());
+   System.out.println(iter.next());
+   System.out.println(iter.next());
+
+   try {
+      iter.next();
+      fail("Should have thrown an exception!");
+    } catch (NoSuchElementException e) {
+      //
+    }
+
+ }
+
 }
